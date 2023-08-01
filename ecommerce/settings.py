ab01443d73aecf4e1b94b110f334f6113212a9d4
Tuki998 <48856004+Tuki998@users.noreply.github.com>
2023-08-01 22:32:03 +0100

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
+STATIC_ROOT = os.path.join(BASE_DIR, 'asests')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
